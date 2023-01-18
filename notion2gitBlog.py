d072e1d03dbef98e4bc759b99a14926717247f1c
uuuseok <useok2791@gmail.com>
2023-01-18 11:14:42 +0900

    [ver 0.1]

--- notion2gitBlog.py
+++ notion2gitBlog.py
@@ -19,7 +19,7 @@ banner:
   background: "#000"
   height: "40vh"
   min_height: "10vh"
-  heading_style: "font-size: 4em; font-weight: bold;"
+  heading_style: "font-size: 2.5rem; font-weight: bold;"
   subheading_style: "color: gold"
 categories: [{}]
 tags: [{}]
@@ -27,102 +27,102 @@ sidebar: [article-menu,category-list]
 ---
 """
 
-
-
 parser = argparse.ArgumentParser()
-parser.add_argument("--title", "-t", type=str, default="제목없음",help="title")
-parser.add_argument("--subtitle", "-s", type=str, default="부제없음",help="subtitle")
-parser.add_argument("--categories", "-c", type=str, default="카테고리없음",help="categories")
-parser.add_argument("--tags", "-tag", type=str, default="태그없음",help="tags")
-
-
+parser.add_argument("--title", "-t", type=str, default="",help="title")
+parser.add_argument("--subtitle", "-s", type=str, default="",help="subtitle")
+parser.add_argument("--categories", "-c", type=str, default="",help="categories")
+parser.add_argument("--tags", "-tag", type=str, default="",help="tags")
 args,_ = parser.parse_known_args()
 
 
 
-def notion2gitblog(title:str, subtitle:str, categories:str, tags:str):
-    
-    ##### date & fileName #####
-    fileName = datetime.now(timezone('Asia/Seoul')).strftime("%Y-%m-%d-")+title+".md"
 
-    ##### unzip #####
-    # zip 파일 경로
-    zip_path = glob("/Users/factorysunny/Desktop/useok/gitBlog/exportNotionPage/*.zip")[0]    
-    
+
+def notion2gitblog(title:str, subtitle:str, categories:str, tags:str):
+    # date & fileName #
+    fileName = datetime.now(timezone("Asia/seoul")).strftime("%Y-%m-%d-%H%M%S-")+title+".md"
+
+    # notion2gitblog, exportNotionPage, {username}.github.io 디렉토리가가 있는 경로의 부모 디렉토리
+    parDir = os.path.abspath(os.path.join(os.getcwd(),os.pardir))
+
+
+    ##### unzip & remove zip file #####
+    # notion에서 export 한 zip 파일 경로
+    zip_path = glob(parDir+"/exportNotionPage/*.zip")[0]
+
     # 압축 해제
     notion_zip = zipfile.ZipFile(zip_path)
-    notion_zip.extractall('/Users/factorysunny/Desktop/useok/gitBlog/exportNotionPage')
+    notion_zip.extractall(parDir+"/exportNotionPage")
     notion_zip.close()
-    
+
     # zip 파일 삭제
     os.remove(zip_path)
-    
+
+
     ##### 경로 이동 #####
-    #이미지,파일 폴더 경로
-    folder_path = glob("/Users/factorysunny/Desktop/useok/gitBlog/exportNotionPage/[!uuuseok]*[!.md]")
+    # 이미지 폴더 경로
+    folder_path = glob(parDir+"/exportNotionPage/*[!.md]")
+
 
     #폴더를 블로그 내부 경로로 이동
     if folder_path == []:
-        pass
+        pass # 이미지 폴더가 없다면 pass
     else:
-        # 폴더 이동
-        shutil.move(folder_path[0], "/Users/factorysunny/Desktop/useok/gitBlog/uuuseok.github.io/assets/images/postImages/{}".format(fileName[:-3]))
+        # 이미지 폴더를 블로그 내부 경로로 이동
+        img_path = glob(parDir+"/[!notion2gitblog|!exportNotionPage]*")[0]+"/assets/images/postImages/{}".format(fileName[:-3])
+        shutil.move(folder_path[0], img_path)
 
-        #폴더 안 이미지를 순서대로 정렬
-        image_path = sorted(glob("/Users/factorysunny/Desktop/useok/gitBlog/uuuseok.github.io/assets/images/postImages/{}/*".format(fileName[:-3])))
+        # 폴더 조회를 위한 폴더처음 %Y-%m-%d-%H%M%S 문자
+        createTime = "-".join(img_path.split('/')[-1].split('-')[:4])
+
+        # 폴더 안 이미지를 순서대로 정렬 [Untitled.png, Untitled 1.png, Untitled 2.png, ...]
+        sorted_img_path = glob(os.path.abspath(os.path.join(img_path,os.pardir))+"/{}*/*".format(createTime))
+        ordered_li = list(map(lambda x : x.split('/')[-1].split('.')[0][9:], sorted_img_path))
+        ordered_li[-1] = '0'
+        ordered_li = list(map(int,ordered_li))
+        sorted_img_path = sorted(sorted_img_path, key=lambda x: dict(zip(sorted_img_path,ordered_li))[x])
 
 
+    # md 파일 경로
+    md_path = glob(parDir+"/exportNotionPage/*md")[0]
 
-        img_li = list(map(lambda x : x.split('/')[-1].split('.')[0][9:], image_path))
-        img_li[-1] = '0'
-        img_li = list(map(lambda x: int(x), img_li))
-        sorted_img_li = sorted(image_path, key=lambda x: dict(zip(image_path,img_li))[x])
-        
-        
-    #md 파일 경로
-    md_path = glob("/Users/factorysunny/Desktop/useok/gitBlog/exportNotionPage/[!uuuseok]*.md")[0]
-    
-    
-    ##### open file #####
+    # md 파일 읽기
     with open(md_path) as f:
         text = f.read()
     
     # md 파일 삭제
     os.remove(md_path)
-    
-    ##### 수정 #####
-    
-    # 이미지 첨부 수정
+
+
+    # md 파일에서 작성된 이미지 첨부 문자 조회
     start = [x.start() for x in re.finditer("!\[Untitled\]\(",text)] # 이미지 첨부 문자 시작 위치
     end = [x.end() for x in re.finditer("png[)]",text)] # 이미지 첨부 문자 끝 위치
-    
     old_path_li = [text[start[x]:end[x]] for x in range(len(start))] 
-    
+
+
+    # md 파일에서 첨부된 이미지를 변경된 이미지의 경로로 변환
     try:
         for i in range(len(start)): 
-            text = text.replace(old_path_li[i], "![Untitled]("+"/"+"/".join(sorted_img_li[i].split('/')[7:])+")") # 새 경로로 변환
+            text = text.replace(old_path_li[i], "![Untitled]("+"/"+"/".join(sorted_img_path[i].split('/')[-5:])+")")
     except:
         print("이미지 없음")
         pass
-    
-    # "#"+1 
+
+
+    # 내 테마에서만 그런건지 모르겠지만, header가 ## 부터 시작함. # 하나를 추가
     text = text.replace("####",'❶❶❶❶❶').replace("###","❷❷❷❷").replace("##","❸❸❸").replace("#",'❹❹').\
     replace("❶❶❶❶❶","#####").replace("❷❷❷❷","####").replace("❸❸❸","###").replace("❹❹",'##')
-    
-    
-    
-    ##### yfm #####
+
+
+    # yfm 
     global yfm
     yfm = yfm.format(title, subtitle, categories, tags)
     text = yfm + text
-    
-    
-    
-    
-    ##### save #####
-    with open("/Users/factorysunny/Desktop/useok/gitBlog/uuuseok.github.io/_posts/"+fileName,'w') as f:
+
+    # .../_posts 디렉토리에 변경된 md파일 저장
+    with open(glob(parDir+"/[!notion2gitblog|!exportNotionPage]*")[0]+"/_posts/"+fileName,'w') as f:
         f.write(text)
-    
+
 
 if __name__ == "__main__":
     notion2gitblog(args.title, args.subtitle, args.categories, args.tags)
